b9937265032343697b6023ddc5e0c7639f5046eb
Illarionov Mykyta <nikitaillarionov2018@gmail.com>
2024-07-13 21:36:25 +0300

    додав при натиску на кнопку перехід на потрібну сторінку

--- WebSitePart/scripts/scriptIndex.js
+++ WebSitePart/scripts/scriptIndex.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function() {
-    var bookingButton = document.getElementById('bookingButton');
-    var headerBookingButtonContainer = document.getElementById('headerBookingButtonContainer');
-    var main = document.querySelector('.main');
+    let bookingButton = document.getElementById('bookingButton');
+    let headerBookingButtonContainer = document.getElementById('headerBookingButtonContainer');
+    let main = document.querySelector('.main');
     
     if (window.scrollY > (main.offsetHeight - bookingButton.offsetHeight - 100)) {
         if (!headerBookingButtonContainer.contains(bookingButton)) {
@@ -16,9 +16,14 @@ window.addEventListener('scroll', function() {
 
 
 
+document.getElementById('bookingButton').addEventListener('click', function() {
+    window.location.href = "../Htmls/ordering.html";
+});
+
+
 //slider
 document.addEventListener('DOMContentLoaded', function () {
-    var swiper = new Swiper('.swiper-container', {
+    let swiper = new Swiper('.swiper-container', {
         slidesPerView: 1,
         spaceBetween: 10,
        
