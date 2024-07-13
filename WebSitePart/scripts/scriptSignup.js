e3696bf7f78836dd84e80432221a2ee2eb5b7bf5
Illarionov Mykyta <nikitaillarionov2018@gmail.com>
2024-07-13 20:47:21 +0300

    зробив адаптивну до розмірів екрану сторінку. Та при натиску на кнопку зберігання данних користувача у змінні.

--- WebSitePart/scripts/scriptSignup.js
+++ WebSitePart/scripts/scriptSignup.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("signup").classList.add("unfolded");
-  });
+
+
+
+
+    document.getElementById("signupButton").addEventListener("click", function() {
+      let username = document.getElementById("username").value;
+      let password = document.getElementById("password").value;
+      let email = document.getElementById("email").value;
+        
+
+
+
+
+
+      
+      document.getElementById("username").value = "";
+      document.getElementById("password").value = "";
+      document.getElementById("email").value = "";
+    });
+    
+    
+    
+});
