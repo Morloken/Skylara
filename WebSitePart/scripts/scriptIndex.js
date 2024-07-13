b880ce63a732e9eb66e977e063d6704918b3e5c8
Illarionov Mykyta <nikitaillarionov2018@gmail.com>
2024-07-13 11:33:14 +0300

    кнопка бронювання тепер закріплюється в хеадері якщокористувач проскролив вниз і вже не бачить її. Створив папку для всіх скриптів. Підлючив скрипт до index.html. Підкорегував вирівнювання елементів UL списку в хеадері, через нову появу кнопки.

---
+++ WebSitePart/scripts/scriptIndex.js
@@ -0,0 +1,15 @@
+window.addEventListener('scroll', function() {
+    var bookingButton = document.getElementById('bookingButton');
+    var headerBookingButtonContainer = document.getElementById('headerBookingButtonContainer');
+    var main = document.querySelector('.main');
+    
+    if (window.scrollY > main.offsetHeight) {
+        if (!headerBookingButtonContainer.contains(bookingButton)) {
+            headerBookingButtonContainer.appendChild(bookingButton);
+        }
+    } else {
+        if (!main.contains(bookingButton)) {
+            document.querySelector('.ordering').appendChild(bookingButton);
+        }
+    }
+});
