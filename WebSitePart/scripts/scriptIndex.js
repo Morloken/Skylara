65c49582c38f4aa09b6d66ff9d6faa73295eea89
Illarionov Mykyta <nikitaillarionov2018@gmail.com>
2024-07-13 21:53:18 +0300

    додав анімації при перегляді блоків користувачем

--- WebSitePart/scripts/scriptIndex.js
+++ WebSitePart/scripts/scriptIndex.js
@@ -49,3 +49,31 @@ window.addEventListener('resize', function() {
         navUl.classList.remove('mobile-visible');
     }
 });
+
+
+
+/*MagicLib */
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+  // Function to add animation class when element is in viewport
+  function animateOnScroll() {
+    const elements = document.querySelectorAll('.magictime');
+    elements.forEach((element) => {
+      if (isInViewport(element)) {
+        element.classList.add('visible');
+      }
+    });
+  }
+  window.addEventListener('scroll', animateOnScroll);
+
+// Initial check
+animateOnScroll();
