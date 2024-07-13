a7588202061563ddac3975274813651bce2dea07
Illarionov Mykyta <nikitaillarionov2018@gmail.com>
2024-07-13 12:54:09 +0300

    Додав інтерактивний слайдер що перемикатиме відгуки користувачів.

--- WebSitePart/scripts/scriptIndex.js
+++ WebSitePart/scripts/scriptIndex.js
@@ -13,3 +13,26 @@ window.addEventListener('scroll', function() {
         }
     }
 });
+
+
+
+//slider
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        autoplay: {
+            delay: 2500,
+        },
+        loop: true,
+    });
+});
+//slider end
