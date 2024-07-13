5f174030468f8c58b3d4052d2cc76ab3b519a589
Illarionov Mykyta <nikitaillarionov2018@gmail.com>
2024-07-13 18:49:18 +0300

    прибрав зайвий код з джс файлу

--- WebSitePart/scripts/scriptOrdering.js
+++ WebSitePart/scripts/scriptOrdering.js
@@ -12,18 +12,4 @@ window.addEventListener('resize', function() {
 });
 
 
-window.addEventListener('scroll', function() {
-    var bookingButton = document.getElementById('bookingButton');
-    var headerBookingButtonContainer = document.getElementById('headerBookingButtonContainer');
-    var main = document.querySelector('.main');
-    
-    if (window.scrollY > (main.offsetHeight - bookingButton.offsetHeight - 100)) {
-        if (!headerBookingButtonContainer.contains(bookingButton)) {
-            headerBookingButtonContainer.appendChild(bookingButton);
-        }
-    } else {
-        if (!main.contains(bookingButton)) {
-            document.querySelector('.ordering').appendChild(bookingButton);
-        }
-    }
-});
+
