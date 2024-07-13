728fc49a673f1070c09f73592f7b7974f6272ee4
Illarionov Mykyta <nikitaillarionov2018@gmail.com>
2024-07-13 16:19:06 +0300

    додав меню що з'являється при малому розмірі вікна сайту, відкриває всі елементи списку в хеадері при натисканні. Додав медіазапити до всього сайту для адаптації під різні розміри екрану користувача.

--- WebSitePart/scripts/scriptIndex.js
+++ WebSitePart/scripts/scriptIndex.js
@@ -30,3 +30,17 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 //slider end
+
+
+//toogle menu
+function toggleMenu() {
+    const navUl = document.querySelector('.navUl');
+    navUl.classList.toggle('mobile-visible');
+}
+
+window.addEventListener('resize', function() {
+    const navUl = document.querySelector('.navUl');
+    if (window.innerWidth > 768) {
+        navUl.classList.remove('mobile-visible');
+    }
+});
