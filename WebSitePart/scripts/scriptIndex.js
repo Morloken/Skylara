76fc5f9239ade516033b3544a004abbb87b8eb27
Illarionov Mykyta <nikitaillarionov2018@gmail.com>
2024-07-14 15:21:56 +0300

    додав стрілку прокрутки на початок сайту

--- WebSitePart/scripts/scriptIndex.js
+++ WebSitePart/scripts/scriptIndex.js
@@ -77,3 +77,27 @@ function isInViewport(element) {
 
 // Initial check
 animateOnScroll();
+
+
+
+
+
+
+
+// Scroll to Top Button
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 300) {
+        scrollToTopBtn.style.display = 'block';
+    } else {
+        scrollToTopBtn.style.display = 'none';
+    }
+});
+
+scrollToTopBtn.addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
