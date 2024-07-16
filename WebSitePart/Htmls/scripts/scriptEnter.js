904499f62d1c2ee61f45c26aafbac41b2ffaaa6e
Illarionov Mykyta <nikitaillarionov2018@gmail.com>
2024-07-16 11:06:22 +0300

    корегування з входом логіки

--- WebSitePart/Htmls/scripts/scriptEnter.js
+++ WebSitePart/Htmls/scripts/scriptEnter.js
@@ -6,35 +6,31 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
     const enterButton = document.getElementById("enterButton");
-    if (enterButton) {
-        enterButton.addEventListener("click", function(event) {
-            event.preventDefault(); // Запобігання стандартій поведінці форми
+if (enterButton) {
+    enterButton.addEventListener("click", function(event) {
+        event.preventDefault(); // Запобігання стандартній поведінці форми
 
-            
-            const passwordInput = document.getElementById("enterPassword");
-            const emailInput = document.getElementById("enterEmail");
+        const passwordInput = document.getElementById("enterPassword");
+        const emailInput = document.getElementById("enterEmail");
 
-            
-            let password = passwordInput.value.trim();
-            let email = emailInput.value.trim();
+        let password = passwordInput.value.trim();
+        let email = emailInput.value.trim();
 
-            
-            if(localStorage.getItem(password) == password && localStorage.getItem(email) == email) {
-                window.location.href = "main.html";
-            }else{
-                let answer = confirm("Невірний логін або пароль. \n\nЧи хочете зареєструватися?");
-                if(answer) {
-                    window.location.href = "index.html";
-                }
+        // Отримання даних з localStorage
+        let storedPassword = localStorage.getItem('userPassword');
+        let storedEmail = localStorage.getItem('userEmail');
+
+        // Перевірка даних
+        if (storedPassword === password && storedEmail === email) {
+            window.location.href = "main.html";
+        } else {
+            let answer = confirm("Невірний логін або пароль. \n\nЧи хочете зареєструватися?");
+            if (answer) {
+                window.location.href = "index.html";
             }
-            
-            
-
-           
-
-            
-        });
-    } 
+        }
+    });
+}
 });
 
 
