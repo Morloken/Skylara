60e999f36812b4f957bd55b8c429fa9a8b7f8138
Illarionov Mykyta <nikitaillarionov2018@gmail.com>
2024-07-16 11:13:42 +0300

    adding checking in console

--- WebSitePart/Htmls/scripts/scriptEnter.js
+++ WebSitePart/Htmls/scripts/scriptEnter.js
@@ -1,52 +1,53 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("enter").classList.add("unfolded");
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("enter").classList.add("unfolded");
 
+  const enterButton = document.getElementById("enterButton");
+  if (enterButton) {
+    enterButton.addEventListener("click", function (event) {
+      event.preventDefault(); // Запобігання стандартній поведінці форми
 
+      const passwordInput = document.getElementById("enterPassword");
+      const emailInput = document.getElementById("enterEmail");
 
+      let password = passwordInput.value.trim();
+      let email = emailInput.value.trim();
 
+      // Отримання даних з localStorage
+      let storedPassword = localStorage.getItem("userPassword");
+      let storedEmail = localStorage.getItem("userEmail");
 
-    const enterButton = document.getElementById("enterButton");
-if (enterButton) {
-    enterButton.addEventListener("click", function(event) {
-        event.preventDefault(); // Запобігання стандартній поведінці форми
-
-        const passwordInput = document.getElementById("enterPassword");
-        const emailInput = document.getElementById("enterEmail");
-
-        let password = passwordInput.value.trim();
-        let email = emailInput.value.trim();
-
-        // Отримання даних з localStorage
-        let storedPassword = localStorage.getItem('userPassword').valueOf();
-        let storedEmail = localStorage.getItem('userEmail').valueOf();
-
-        // Перевірка даних
-        if (storedPassword == password && storedEmail == email) {
-            window.location.href = "main.html";
+      // Перевірка даних
+      if (storedPassword == password) {
+        console.log("Password match");
+        if (storedEmail == email) {
+          console.log("Email match");
+          window.location.href = "main.html";
         } else {
-            let answer = confirm("Невірний логін або пароль. \n\nЧи хочете зареєструватися?");
-            if (answer) {
-                window.location.href = "index.html";
-            }
+          let answer = confirm(
+            "Невірний логін або пароль. \n\nЧи хочете зареєструватися?"
+          );
+          if (answer) {
+            window.location.href = "index.html";
+          }
         }
+      }
     });
-}
+  }
 });
 
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
-const scrollToTopBtn = document.getElementById('scrollToTopBtn');
-
-window.addEventListener('scroll', function() {
-    if (window.scrollY > 300) {
-        scrollToTopBtn.style.display = 'block';
-    } else {
-        scrollToTopBtn.style.display = 'none';
-    }
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 300) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
 });
 
-scrollToTopBtn.addEventListener('click', function() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+scrollToTopBtn.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
