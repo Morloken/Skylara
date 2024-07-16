cbbae319665a3b9ef8fbda169742543d2596c8d8
Illarionov Mykyta <nikitaillarionov2018@gmail.com>
2024-07-16 08:37:58 +0300

    створив механіку Входу або перекидання на вікно Реєстрації, якщо користувач не заходив до цього.

--- WebSitePart/scripts/scriptEnter.js
+++ WebSitePart/scripts/scriptEnter.js
@@ -2,10 +2,39 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("enter").classList.add("unfolded");
 
 
-    
 
 
 
+    const enterButton = document.getElementById("enterButton");
+    if (enterButton) {
+        enterButton.addEventListener("click", function(event) {
+            event.preventDefault(); // Запобігання стандартій поведінці форми
+
+            
+            const passwordInput = document.getElementById("enterPassword");
+            const emailInput = document.getElementById("enterEmail");
+0
+            
+            let password = passwordInput.value.trim();
+            let email = emailInput.value.trim();
+
+            
+            if(localStorage.getItem(password) === password && localStorage.getItem(email) === email) {
+                window.location.href = "../Htmls/index.html";
+            }else{
+                let answer = confirm("Невірний логін або пароль. \n\nЧи хочете зареєструватися?");
+                if(answer) {
+                    window.location.href = "../Htmls/signup.html";
+                }
+            }
+            
+            
+
+           
+
+            
+        });
+    } 
 });
 
 
