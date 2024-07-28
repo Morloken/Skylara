1a3860582435d89fd8d560abdbafc38c3dd4d3d7
Illarionov Mykyta <nikitaillarionov2018@gmail.com>
2024-07-28 21:22:41 +0300

    Додавання статистики відвідування і роботи сайту

---
+++ WebSitePart/Htmls/layout.js
@@ -0,0 +1,17 @@
+import {Analytics} from '@vercel/analytics/react';
+
+
+
+export default function RootLayout({ children }) {
+    return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
